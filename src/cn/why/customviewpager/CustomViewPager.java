33eeea70198e7785fe8599b3c9738364940a06f1
why20104712 <1062383915@qq.com>
2014-11-22 20:43:43 +0800

    新增第三个子view为listview，实现listview可以上下滑动和左右滑动 使用onInterceptTouchEvent方法进行时间的拦截处理，在该方法中判断down事件和move事件，确定x和y轴的移动距离 然后将x轴与y轴的移动距离进行比较，确定是水平移动还是垂直移动，从而进行设置拦截的状态变量的值为true或者false

--- src/cn/why/customviewpager/CustomViewPager.java
+++ src/cn/why/customviewpager/CustomViewPager.java
@@ -9,16 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Scroller;
 
-public class CustomViewPager extends ViewGroup{
+public class CustomViewPager extends ViewGroup {
 
 	private GestureDetector gestureDetector;
 	private Context context;
-//	private CustomScoller scoller;
+	// private CustomScoller scoller;
 	private Scroller scoller;
 	private boolean isFling;
 	private int currentViewId = 0;
 	private float startX;
 	private int nextId = 0;
+
 	public CustomViewPager(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
@@ -26,48 +27,56 @@ public class CustomViewPager extends ViewGroup{
 	}
 
 	private void initView() {
-//			scoller = new CustomScoller(context);
-			scoller = new Scroller(context);
-			gestureDetector = new GestureDetector(context, new OnGestureListener() {
-				@Override
-				public boolean onSingleTapUp(MotionEvent arg0) {
-					return false;
+		// scoller = new CustomScoller(context);
+		scoller = new Scroller(context);
+		gestureDetector = new GestureDetector(context, new OnGestureListener() {
+			@Override
+			public boolean onSingleTapUp(MotionEvent arg0) {
+				return false;
+			}
+
+			@Override
+			public void onShowPress(MotionEvent arg0) {
+			}
+
+			@Override
+			public boolean onScroll(MotionEvent e1, MotionEvent e2,
+					float distanceX, float distanceY) {
+				scrollBy((int) distanceX, 0);
+				return false;
+			}
+
+			public void onLongPress(MotionEvent arg0) {
+			}
+
+			/**
+			 * 快速滑动事件
+			 */
+			public boolean onFling(MotionEvent arg0, MotionEvent arg1,
+					float velocityX, float velocityY) {
+				isFling = true;
+				if (velocityX > 0 && currentViewId > 0) { // 快速向右滑动
+					currentViewId--;
+				} else if (velocityX < 0 && currentViewId < getChildCount() - 1) { // 快速向左滑动
+					currentViewId++;
 				}
-				@Override
-				public void onShowPress(MotionEvent arg0) {}
-				@Override
-				public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-						float distanceY) {
-					scrollBy((int) distanceX, 0);
-					return false;
-				}
-				public void onLongPress(MotionEvent arg0) {}
-				/**
-				 * 快速滑动事件
-				 */
-				public boolean onFling(MotionEvent arg0, MotionEvent arg1, float velocityX,	float velocityY) {
-					isFling = true;
-					if(velocityX>0 && currentViewId>0){ // 快速向右滑动
-						currentViewId--;
-					}else if(velocityX<0 && currentViewId<getChildCount()-1){ // 快速向左滑动
-						currentViewId++;
-					}
-					moveToDest(currentViewId);
-					return false;
-				}
-				
-				@Override
-				public boolean onDown(MotionEvent arg0) {
-					return false;
-				}
-			});
+				moveToDest(currentViewId);
+				return false;
+			}
+
+			@Override
+			public boolean onDown(MotionEvent arg0) {
+				return false;
+			}
+		});
 	}
 
 	@Override
 	protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
 		for (int i = 0; i < getChildCount(); i++) {
-			View  view  = getChildAt(i);
-			view.layout(0+i*getWidth(), 0, getWidth()+i*getWidth(), getHeight());
+			View view = getChildAt(i);
+			view.layout(0 + i * getWidth(), 0, getWidth() + i * getWidth(),
+					getHeight());
 		}
 	}
 
@@ -75,43 +84,52 @@ public class CustomViewPager extends ViewGroup{
 	public boolean onTouchEvent(MotionEvent event) {
 		super.onTouchEvent(event);
 		gestureDetector.onTouchEvent(event);
-		
+
 		switch (event.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				startX = event.getX();
-				break;
-			case MotionEvent.ACTION_MOVE:
-				
-				break;
-			case MotionEvent.ACTION_UP:
-				if (!isFling) {
-					if (event.getX() - startX > getWidth()/2) {
-						nextId = currentViewId - 1;
-					}else if (startX - event.getX() > getWidth()/2) {
-						nextId = currentViewId + 1;
-					}else {
-						nextId = currentViewId;
-					} 
-					nextId = (nextId >=0)?nextId:0;//nextId不可小于0
-					currentViewId = (nextId >= 0)?nextId:0;
-					currentViewId = (nextId <= getChildCount() - 1)?nextId:(getChildCount() - 1);
-					moveToDest(nextId);
+		case MotionEvent.ACTION_DOWN:
+			startX = event.getX();
+			break;
+		case MotionEvent.ACTION_MOVE:
+
+			break;
+		case MotionEvent.ACTION_UP:
+			if (!isFling) {
+				if (event.getX() - startX > getWidth() / 2) {
+					nextId = currentViewId - 1;
+				} else if (startX - event.getX() > getWidth() / 2) {
+					nextId = currentViewId + 1;
+				} else {
+					nextId = currentViewId;
 				}
-				isFling = false;
-				break;
+				moveToDest(nextId);
 			}
-		
+			isFling = false;
+			break;
+		}
+
 		return true;
 	}
-	private void moveToDest(int nextId) {
-		int distance = currentViewId*getWidth() - getScrollX();//移动的距离 = 最终位置 - 开始位置
+
+	/**
+	 * 移动到指定页
+	 * 
+	 * @param nextId
+	 */
+	public void moveToDest(int nextId) {
+		nextId = (nextId >= 0) ? nextId : 0;// nextId不可小于0
+		currentViewId = (nextId >= 0) ? nextId : 0;
+		currentViewId = (nextId <= getChildCount() - 1) ? nextId
+				: (getChildCount() - 1);
+		int distance = currentViewId * getWidth() - getScrollX();// 移动的距离 = 最终位置
+																	// - 开始位置
 		scoller.startScroll(getScrollX(), 0, distance, 0);
-		//触发listener事件
-		if(pageChangedListener!=null){
+		// 触发listener事件
+		if (pageChangedListener != null) {
 			pageChangedListener.moveToDest(nextId);
 		}
 		invalidate();
 	}
+
 	/**
 	 * 每次执行invalidate方法，都会调用computeScroll方法
 	 */
@@ -123,21 +141,63 @@ public class CustomViewPager extends ViewGroup{
 			invalidate();
 		}
 	}
+
 	private CustomPageChangedListener pageChangedListener;
+	private float startY;
+
 	public CustomPageChangedListener getPageChangedListener() {
 		return pageChangedListener;
 	}
 
-	public void setPageChangedListener(CustomPageChangedListener pageChangedListener) {
+	public void setPageChangedListener(
+			CustomPageChangedListener pageChangedListener) {
 		this.pageChangedListener = pageChangedListener;
 	}
+
 	/**
 	 * 自定义页面变化事件,监听接口
+	 * 
 	 * @author why
-	 *
+	 * 
 	 */
-	public interface CustomPageChangedListener{
+	public interface CustomPageChangedListener {
 		public void moveToDest(int currId);
 	}
-	
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		for (int i = 0; i < getChildCount(); i++) {
+			View view = getChildAt(i);
+			view.measure(widthMeasureSpec, heightMeasureSpec);
+		}
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		boolean result = false;
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			gestureDetector.onTouchEvent(ev);
+			startX = ev.getX();
+			startY = ev.getY();
+			break;
+
+		case MotionEvent.ACTION_MOVE:
+			float distanceX = Math.abs(ev.getX() - startX);
+			float distanceY = Math.abs(ev.getY() - startY);
+			
+			if (distanceX > distanceY && distanceX > 10) {
+				result = true;
+			}else if (distanceX < distanceY && distanceY > 10 ) {
+				result = false;
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+
+			break;
+		}
+		return result;
+	}
+
 }
--- src/cn/why/customviewpager/MainActivity.java
+++ src/cn/why/customviewpager/MainActivity.java
@@ -1,32 +1,88 @@
 package cn.why.customviewpager;
 
-import cn.why.customviewpager.CustomViewPager.CustomPageChangedListener;
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Toast;
+import cn.why.customviewpager.CustomViewPager.CustomPageChangedListener;
 
 public class MainActivity extends Activity {
 
 	private CustomViewPager customViewPager;
+	private RadioGroup radioGroup;
+	private ListView listView;
 	private int[] ids = new int[]{R.drawable.a1,R.drawable.a2,R.drawable.a3,R.drawable.a4,R.drawable.a5,R.drawable.a6};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main);
         customViewPager = (CustomViewPager) findViewById(R.id.viewPager);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         for (int i = 0; i < ids.length; i++) {
         	ImageView imageView = new ImageView(this);
         	imageView.setBackgroundResource(ids[i]);
         	customViewPager.addView(imageView);
+        	
+        	RadioButton radioButton = new RadioButton(this);
+        	radioButton.setId(i);
+        	radioGroup.addView(radioButton);
+        	if (i == 0) {
+        		radioButton.setChecked(true);
+        	}
 		}
+      //给自定义viewGroup添加测试的布局
+  		View temp = getLayoutInflater().inflate(R.layout.temp, null);
+  		customViewPager.addView(temp, 2);
+  		listView = (ListView) findViewById(R.id.listView1);
+  		listView.setAdapter(new CustomListViewAdapter());
         //使用自定义的页面变化事件
         customViewPager.setPageChangedListener(new CustomPageChangedListener() {
 			public void moveToDest(int currId) {
 				Toast.makeText(getApplicationContext(), "页面发生了改变", Toast.LENGTH_SHORT).show();
+				((RadioButton)radioGroup.getChildAt(currId)).setChecked(true);
+			}
+		});
+        //为radioGroup设置监听事件
+        radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				customViewPager.moveToDest(checkedId);
 			}
 		});
     }
-    
+    /*
+     * 自定义listview适配器
+     */
+    public class CustomListViewAdapter extends BaseAdapter{
+		@Override
+		public int getCount() {
+			return ids.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return ids[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View view, ViewGroup group) {
+			ImageView imageView = new ImageView(getApplicationContext());
+			imageView.setBackgroundResource(ids[position]);
+			return imageView;
+		}
+    	
+    }
     
 }
